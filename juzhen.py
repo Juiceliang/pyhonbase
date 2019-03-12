894835a940d967b475f6cd8698179e3eba24057e
Juiceliang <48407536+Juiceliang@users.noreply.github.com>
2019-03-12 11:22:21 +0800

    Day01

---
+++ juzhen.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+i = 0
+while i <= 3 :
+    j = 0
+    while j<= 4:
+        print("*",end=" ")
+        j = j+ 1
+    print(" ")
+    i = i + 1
+print("*",end=" ")
+print(" ")
+print("*", end=" ")
