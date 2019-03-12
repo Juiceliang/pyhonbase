7164c951ea94d79625ba3312955205fd5f95ff73
Juiceliang <48407536+Juiceliang@users.noreply.github.com>
2019-03-12 11:21:36 +0800

    Day01

---
+++ 名片.py
@@ -0,0 +1,8 @@
+#coding：utf-8
+name = input('请输入你的姓名：')
+qq = input('请输入你的QQ：')
+tel = input('请输入你的手机号：')
+adr = input('请输入公司地址：')
+print("=================")
+print(" 姓名：%s\n QQ:%s\n 手机号：%s\n 公司地址：%s"%(name,qq,tel,adr))
+print("=================")
